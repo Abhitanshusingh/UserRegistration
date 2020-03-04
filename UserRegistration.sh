c1707f01c200ba665f7acdf0774a423108199ae3
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-04 14:35:44 +0530

    MODIFIED: Added valid first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 printf "Welcome to user registration\n"
+read -p "Enter your first name :" firstName
+firstNamePattern="^[A-Z][a-z]{2}"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	printf "Valid\n"
+else
+	printf "In valid\n"
+fi
