8ab16f410dd0c32b38f8a10b837befa11bd3a216
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-04 15:30:09 +0530

    MODIFIED: Added to check valid email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 printf "Welcome to user registration\n"
 read -p "Enter your first name :" userInput1
-read -p "Enter your last name " userInput2
+read -p "Enter your last name  :" userInput2
+read -p "Enter your email id   :" userInput3
 #First name starts with Cap and has minimum 3 characters
 function isFirstName()
 {
@@ -20,12 +21,24 @@ function isLastName()
 {
 	lastName=$1
 	lastNamePattern="^[A-Z][a-z]{2}"
-	 if [[ $lastName =~ $lastNamePattern ]]
+	if [[ $lastName =~ $lastNamePattern ]]
+	then
+		printf "Valid\n"
+	else
+		printf "In valid\n"
+	fi
+}
+isLastName $userInput2\
+#Logic for to check valid email
+function isEmail()
+{
+	email=$1
+	emailPattern="^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,}"
+	if [[ $email =~ $emailPattern ]]
    then
       printf "Valid\n"
    else
       printf "In valid\n"
    fi
 }
-isLastName $userInput2
-
+isEmail $userInput3
